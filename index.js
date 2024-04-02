f48de0f9c5621fd3d22e10661eee69817a1e6150
jasnmar <jasnmar@gmail.com>
2024-04-02 14:34:58 -0400

    Close Button on Modal

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
+const modalEl = document.getElementById("modal")
+
 setTimeout(function() {
-    const modalEl = document.getElementById("modal")
-    modalEl.style.display = "block";
+    
+    modalEl.style.display = "inline";
     console.log("Modal Opened!")
 }, 1500)
+
+const modalCloseBtn = document.getElementById("modal-close-btn")
+
+modalCloseBtn.addEventListener("click", function() {
+    modalEl.style.display = "none"
+})
