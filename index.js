f64f543b6d04864e078b5f764031ae91abc1232a
jasnmar <jasnmar@gmail.com>
2024-04-02 15:08:36 -0400

    Prevent Default

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ const modalCloseBtn = document.getElementById("modal-close-btn")
 modalCloseBtn.addEventListener("click", function() {
     modalEl.style.display = "none"
 })
+
+const consentForm = document.getElementById("consent-form")
+
+consentForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log("Form Submitted")
+})
